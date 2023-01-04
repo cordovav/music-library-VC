020fd3ab903525528268b4f931a30cae893fc141
Veny Cordova <venycordova@gmail.com>
2023-01-04 17:54:07 -0500

    fixing the datacontext folder and files

---
+++ music-library/src/App.js
@@ -0,0 +1,46 @@
+import Gallery from './Components/Gallery';
+import SearchBar from './Components/SearchBar';
+import { useEffect, useState } from 'react'
+import { DataContext } from './Context/DataContext';
+//import './App.css'
+
+function App() {
+  let [data, setData] = useState([]);
+  let [message, setMessage] = useState("Search for Music!");
+  let [search, setSearch] = useState('');
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(()=>{
+    if(search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+      const response = await fetch(API_URL + search)
+      const resData = await response.json();
+        if (resData.results.length > 0) {
+          setData(resData.results);
+        } else {
+          setMessage("Not Found");
+        }
+      }
+      fetchData();
+    }
+  },[search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
+  return (
+    <div className='App'>
+      <SearchBar handleSearch = {handleSearch} />
+      {message}
+      <DataContext.Provider value = {data}>
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  );
+}
+
+export default App;
